b8c3b26b6b316b6505db3824b4f93cf6860b669a
freelanceranuragsharma <freelanceranuragsharma@gmail.com>
2016-03-02 00:58:45 -0500

    New nav bar for admin and changes to dashboard html

--- public/js/views/admin/adminDashboardView.js
+++ public/js/views/admin/adminDashboardView.js
@@ -1,6 +1,6 @@
 define([
-   'jquery','backbone','handlebars','globalHandlebarsHelper','models/adminDashboard','text!templates/admin/adminDashboard.handlebars'
-   ], function($, Backbone, Handlebars, GlobalHandlebarsHelper, AdminDashboardModel, AdminDashboardTemplate) {
+   'jquery','backbone','bootstrap','handlebars','globalHandlebarsHelper','models/adminDashboard','text!templates/admin/adminDashboard.handlebars'
+   ], function($, Backbone, Bootstrap, Handlebars, GlobalHandlebarsHelper, AdminDashboardModel, AdminDashboardTemplate) {
    var AdminDashboardView = Backbone.View.extend({
       el: '#main-container',
       events: {
@@ -18,6 +18,20 @@ define([
       render: function() {
          if(!_.isEmpty(this.model.attributes)){
             this.$el.html(this.template({data:this.model.attributes}));
+            $('[id^=detail-]').hide();
+            $('.toggle').click(function() {
+               $input = $( this );
+               $target = $('#'+$input.attr('data-toggle'));
+               $target.slideToggle();
+               if($( this ).find('#dropdownArrow').hasClass('fa-chevron-right')){
+                  $( this ).find('#dropdownArrow').removeClass('fa-chevron-right').addClass('fa-chevron-down');
+                  $( this ).find('#dropdownArrow').parent()[0].style.background='blue'
+               }else{
+                  $( this ).find('#dropdownArrow').removeClass('fa-chevron-down').addClass('fa-chevron-right');
+                  $( this ).find('#dropdownArrow').parent()[0].style.background='red'
+               }
+               
+            });
             $("a[href='#dashboard']").append('<div class="pointer"><div class="arrow"></div><div class="arrow_border"></div></div>')
             $('#sidebar-nav').show();
             return this;
